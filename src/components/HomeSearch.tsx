af01451d0059c8a4a4c7a89f62cace7859254e41
Ogbo Friday <hedosphere@gmail.com>
2023-06-21 17:58:29 +0100

    Refine

--- src/components/HomeSearch.tsx
+++ src/components/HomeSearch.tsx
@@ -3,8 +3,8 @@ import { BsSearch, BsMicFill, BsCamera } from "react-icons/bs";
 
 const HomeSearch = () => {
   return (
-    <div className="mt-6">
-      <form className="sm:min-w-[30rem] md:min-w-[40rem] min-w-[22rem] text-lg  flex border gap-3 rounded-full items-center p-3 focus-within:shadow-lg hover:shadow-lg">
+    <div className="mt-6 ">
+      <form className="sm:min-w-[30rem] md:min-w-[40rem] min-w-[90vw] mx-auto text-lg  flex border gap-3 rounded-full items-center py-1 px-3 sm:p-3  focus-within:shadow-lg hover:shadow-lg">
         <BsSearch className=" select-none opacity-40 text-sm" />
         <input
           autoFocus={true}
